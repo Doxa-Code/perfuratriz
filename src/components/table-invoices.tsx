ecf003bcfb1f6b16ef42a034175258f4f24e7ff8
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-05-22 12:55:40 -0300

    feat: sort table di invoice

--- src/components/table-invoices.tsx
+++ src/components/table-invoices.tsx
@@ -12,127 +12,128 @@ import { TableComponent } from "./table";
 import { Button } from "./ui/button";
 
 type InvoiceRaw = {
-  id: string;
-  registration: string;
-  createdAt: Date;
-  isVinculated: boolean;
-  quote: number;
-  products: {
-    product: Product.Props;
-    quantity: number;
-    amount: number;
-  }[];
+	id: string;
+	registration: string;
+	createdAt: Date;
+	isVinculated: boolean;
+	quote: number;
+	products: {
+		product: Product.Props;
+		quantity: number;
+		amount: number;
+	}[];
 };
 
 type Props = {
-  invoices: InvoiceRaw[];
+	invoices: InvoiceRaw[];
 };
 
 const columns: ColumnDef<InvoiceRaw>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all rows"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    size: 28,
-    enableSorting: false,
-  },
-  {
-    header: "Número da invoice",
-    accessorKey: "registration",
-    cell: ({ row }) => (
-      <div className="font-medium">{row.getValue("registration")}</div>
-    ),
-    size: 180,
-  },
-  {
-    header: "Data da invoice",
-    accessorKey: "createdAt",
-    cell: ({ row }) => (
-      <div className="font-medium">
-        {(row.getValue("createdAt") as Date).toLocaleDateString("pt-BR")}
-      </div>
-    ),
-  },
-  {
-    header: "Cotação USD",
-    accessorKey: "quote",
-    cell: ({ row }) => (
-      <div className="font-medium">
-        {Number(row.getValue("quote")).toLocaleString("pt-BR", {
-          currency: "USD",
-          style: "currency",
-          minimumFractionDigits: 4,
-        })}
-      </div>
-    ),
-  },
+	{
+		id: "select",
+		header: ({ table }) => (
+			<Checkbox
+				checked={
+					table.getIsAllPageRowsSelected() ||
+					(table.getIsSomePageRowsSelected() && "indeterminate")
+				}
+				onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+				aria-label="Select all rows"
+			/>
+		),
+		cell: ({ row }) => (
+			<Checkbox
+				checked={row.getIsSelected()}
+				onCheckedChange={(value) => row.toggleSelected(!!value)}
+				aria-label="Select row"
+			/>
+		),
+		size: 28,
+		enableSorting: false,
+	},
+	{
+		header: "Número da invoice",
+		accessorKey: "registration",
+		cell: ({ row }) => (
+			<div className="font-medium">{row.getValue("registration")}</div>
+		),
+		size: 180,
+	},
+	{
+		header: "Data da invoice",
+		accessorKey: "createdAt",
+		cell: ({ row }) => (
+			<div className="font-medium">
+				{(row.getValue("createdAt") as Date).toLocaleDateString("pt-BR")}
+			</div>
+		),
+	},
+	{
+		header: "Cotação USD",
+		accessorKey: "quote",
+		cell: ({ row }) => (
+			<div className="font-medium">
+				{Number(row.getValue("quote")).toLocaleString("pt-BR", {
+					currency: "USD",
+					style: "currency",
+					minimumFractionDigits: 4,
+				})}
+			</div>
+		),
+	},
 ];
 
 export const TableInvoices: React.FC<Props> = (props) => {
-  const { setRegister } = useRegisterEdit();
-  const [filter, setFilter] = useState<"ALL" | "VINCULATED" | "NO-VINCULATED">(
-    "ALL"
-  );
-  return (
-    <>
-      <header className="space-x-2">
-        <Button
-          onClick={() => setFilter("ALL")}
-          variant={filter === "ALL" ? "default" : "outline"}
-        >
-          Todas
-        </Button>
-        <Button
-          onClick={() => setFilter("VINCULATED")}
-          variant={filter === "VINCULATED" ? "default" : "outline"}
-        >
-          Vinculadas
-        </Button>
-        <Button
-          onClick={() => setFilter("NO-VINCULATED")}
-          variant={filter === "NO-VINCULATED" ? "default" : "outline"}
-        >
-          Não vinculadas
-        </Button>
-      </header>
-      <TableComponent
-        registers={props.invoices.filter((i) =>
-          filter === "ALL"
-            ? true
-            : filter === "VINCULATED"
-            ? i.isVinculated
-            : !i.isVinculated
-        )}
-        defaultSortingColumn="registration"
-        keyToSearch="registration"
-        modalName={MODAL_CREATE_INVOICE}
-        actionRemove={removeInvoiceAction}
-        onChangeRegister={setRegister}
-        columns={columns}
-        onError={() => {
-          toast.error(
-            "Não foi possível excluir pois há declarações vinculadas a esses registros...",
-            {
-              position: "top-center",
-            }
-          );
-        }}
-      />
-    </>
-  );
+	const { setRegister } = useRegisterEdit();
+	const [filter, setFilter] = useState<"ALL" | "VINCULATED" | "NO-VINCULATED">(
+		"ALL",
+	);
+	return (
+		<>
+			<header className="space-x-2">
+				<Button
+					onClick={() => setFilter("ALL")}
+					variant={filter === "ALL" ? "default" : "outline"}
+				>
+					Todas
+				</Button>
+				<Button
+					onClick={() => setFilter("VINCULATED")}
+					variant={filter === "VINCULATED" ? "default" : "outline"}
+				>
+					Vinculadas
+				</Button>
+				<Button
+					onClick={() => setFilter("NO-VINCULATED")}
+					variant={filter === "NO-VINCULATED" ? "default" : "outline"}
+				>
+					Não vinculadas
+				</Button>
+			</header>
+			<TableComponent
+				registers={props.invoices.filter((i) =>
+					filter === "ALL"
+						? true
+						: filter === "VINCULATED"
+							? i.isVinculated
+							: !i.isVinculated,
+				)}
+				defaultSortingColumn="createdAt"
+				defaultSorting="desc"
+				keyToSearch="registration"
+				modalName={MODAL_CREATE_INVOICE}
+				actionRemove={removeInvoiceAction}
+				onChangeRegister={setRegister}
+				columns={columns}
+				onError={() => {
+					toast.error(
+						"Não foi possível excluir pois há declarações vinculadas a esses registros...",
+						{
+							position: "top-center",
+						},
+					);
+				}}
+			/>
+		</>
+	);
 };
